77d9a9987fdc491c30da920c9583f80bbf2527ae
= <alonsomartins056@gmail.com>
2025-07-08 20:37:10 -0500

    fazendo o formulário preencher a informação na tabela (há algum erro)

--- sgcm/script.js
+++ sgcm/script.js
@@ -6,7 +6,38 @@ function carregarProfissionais() {
             let dados = JSON.parse(xhr.responseText)
             let tabela = document.querySelector('table');
 
-            for (let item of dados);
+            for (let item of dados) {
+                let linha = document.createElement('tr');
+
+                let id = document.createElement('td');
+                let nome = document.createElement('td');
+                let registro = document.createElement('td');
+                let telefone = document.createElement('td');
+                let email = document.createElement('td');
+                let unidade = document.createElement('td');
+                let especialidade = document.createElement('td');
+                let opcoes = document.createElement('td');
+
+                id.textContent = item.id;
+                nome.textContent = item.nome;
+                registro.textContent = item.registro;
+                telefone.textContent = item.telefone;
+                email.textContent = item.email;
+                unidade.textContent = item.unidade;
+                especialidade.textContent = item.especialidade;
+                opcoes.innerHTML = `<a href="" class="botao_verde">Editar</a>|<a href="" class="botao_vermelho">Excluir</a>`;
+
+                linha.appendChild(id);
+                linha.appendChild(nome);
+                linha.appendChild(registro);
+                linha.appendChild(telefone);
+                linha.appendChild(email);
+                linha.appendChild(unidade);
+                linha.appendChild(especialidade);
+                linha.appendChild(opcoes);
+
+                tabela.appendChild(linha);
+            }
         }
     }); // fecha o escutador de evento
     xhr.send();
@@ -14,7 +45,25 @@ function carregarProfissionais() {
 carregarProfissionais();
 console.log("oi");
 
+let form = document.querySelector('form');
+let btn_enviar = document.querySelector('input[type="submit"]');
+btn_enviar.addEventListener('click', () => {
+    // let objeto = new Object();
+    let objeto = {
+        nome: form.nome.value,
+        registro: form.registroConselho.value,
+        telefone: form.telefone.value,
+        email: form.email.value,
+        unidade: form.unidade.value,
+        especialidade: form.especialidade.value
+    }
+
+    inserirProfissional(objeto);
+});
+
 const inserirProfissional = (item) => {
+    console.log("Inserindo:",item);
+    let tabela = document.querySelector('table');
     let linha = document.createElement('tr');
 
     let id = document.createElement('td');
@@ -45,4 +94,6 @@ const inserirProfissional = (item) => {
     linha.appendChild(opcoes);
 
     tabela.appendChild(linha);
+
+    console.log("Objeto inserido:",item);
 }
