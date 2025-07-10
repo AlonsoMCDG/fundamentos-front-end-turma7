1801349c575c2d759b5c69b0786178f57b4d5f03
= <alonsomartins056@gmail.com>
2025-07-09 19:19:53 -0500

    não lembro o que foi adicionado

--- sgcm/script.js
+++ sgcm/script.js
@@ -3,67 +3,73 @@ function carregarProfissionais() {
     xhr.open('GET', 'http://my-json-server.typicode.com/juniorlimeiras/json/profissionais')
     xhr.addEventListener('readystatechange', () => {
         if (xhr.readyState == 4 && xhr.status == 200) {
-            let dados = JSON.parse(xhr.responseText)
-            let tabela = document.querySelector('table');
+            let dados = JSON.parse(xhr.responseText);
 
             for (let item of dados) {
-                let linha = document.createElement('tr');
+                inserirProfissional(item);
+                
+                // let linha = document.createElement('tr');
 
-                let id = document.createElement('td');
-                let nome = document.createElement('td');
-                let registro = document.createElement('td');
-                let telefone = document.createElement('td');
-                let email = document.createElement('td');
-                let unidade = document.createElement('td');
-                let especialidade = document.createElement('td');
-                let opcoes = document.createElement('td');
+                // let id = document.createElement('td');
+                // let nome = document.createElement('td');
+                // let registro = document.createElement('td');
+                // let telefone = document.createElement('td');
+                // let email = document.createElement('td');
+                // let unidade = document.createElement('td');
+                // let especialidade = document.createElement('td');
+                // let opcoes = document.createElement('td');
 
-                id.textContent = item.id;
-                nome.textContent = item.nome;
-                registro.textContent = item.registro;
-                telefone.textContent = item.telefone;
-                email.textContent = item.email;
-                unidade.textContent = item.unidade;
-                especialidade.textContent = item.especialidade;
-                opcoes.innerHTML = `<a href="" class="botao_verde">Editar</a>|<a href="" class="botao_vermelho">Excluir</a>`;
+                // id.textContent = item.id;
+                // nome.textContent = item.nome;
+                // registro.textContent = item.registro;
+                // telefone.textContent = item.telefone;
+                // email.textContent = item.email;
+                // unidade.textContent = item.unidade;
+                // especialidade.textContent = item.especialidade;
+                // opcoes.innerHTML = `<a href="javascript:void(0)" class="botao_verde">Editar</a>|<a href="javascript:void(0)" class="botao_vermelho">Excluir</a>`;
 
-                linha.appendChild(id);
-                linha.appendChild(nome);
-                linha.appendChild(registro);
-                linha.appendChild(telefone);
-                linha.appendChild(email);
-                linha.appendChild(unidade);
-                linha.appendChild(especialidade);
-                linha.appendChild(opcoes);
+                // linha.appendChild(id);
+                // linha.appendChild(nome);
+                // linha.appendChild(registro);
+                // linha.appendChild(telefone);
+                // linha.appendChild(email);
+                // linha.appendChild(unidade);
+                // linha.appendChild(especialidade);
+                // linha.appendChild(opcoes);
 
-                tabela.appendChild(linha);
+                // tabela.appendChild(linha);
+                // excluirLinha();
             }
         }
     }); // fecha o escutador de evento
     xhr.send();
 }
+
+let tabela = document.querySelector('table');
+
 carregarProfissionais();
 console.log("oi");
 
+
 let form = document.querySelector('form');
 let btn_enviar = document.querySelector('input[type="submit"]');
-btn_enviar.addEventListener('click', () => {
+btn_enviar.addEventListener('click', (event) => {
+    event.preventDefault() // Evita que a página seja recarregada
     // let objeto = new Object();
     let objeto = {
+        id: tabela.rows.length,
         nome: form.nome.value,
         registro: form.registroConselho.value,
         telefone: form.telefone.value,
         email: form.email.value,
-        unidade: form.unidade.value,
-        especialidade: form.especialidade.value
+        unidade: form.unidade.options[form.unidade.selectedIndex].label,
+        especialidade: form.especialidade.options[form.especialidade.selectedIndex].label
     }
 
     inserirProfissional(objeto);
 });
 
 const inserirProfissional = (item) => {
-    console.log("Inserindo:",item);
-    let tabela = document.querySelector('table');
     let linha = document.createElement('tr');
 
     let id = document.createElement('td');
@@ -82,7 +88,8 @@ const inserirProfissional = (item) => {
     email.textContent = item.email;
     unidade.textContent = item.unidade;
     especialidade.textContent = item.especialidade;
-    opcoes.innerHTML = `<a href="" class="botao_verde">Editar</a>|<a href="" class="botao_vermelho">Excluir</a>`;
+    opcoes.innerHTML = `<a href="javascript:void(0)" class="botao_verde">Editar</a>|<a href="javascript:void(0)" class="botao_vermelho">Excluir</a>`;
+    excluirLinha();
 
     linha.appendChild(id);
     linha.appendChild(nome);
@@ -95,5 +102,17 @@ const inserirProfissional = (item) => {
 
     tabela.appendChild(linha);
 
-    console.log("Objeto inserido:",item);
+    console.log("Objeto inserido:", item);
+}
+
+function excluirLinha() {
+    let botoes_excluir = document.querySelectorAll('a.botao_vermelho')
+    for (let botao of botoes_excluir) {
+        
+        botao.addEventListener('click', (event) => {
+            botao.parentElement.parentElement.remove();
+        });
+    }
+
+    // console.log("evento excluir")
 }
